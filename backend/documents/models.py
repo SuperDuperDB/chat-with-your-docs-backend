f7454d51eb7488b3c15bdd3e15e3e094032496d4
blythed <duncanblythe@gmail.com>
2023-10-12 10:07:17 +0200

    Add code from superduperdb project

---
+++ backend/documents/models.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class Repo(str, Enum):
+    superduperdb = 'superduperdb'
+    langchain = 'langchain'
+    fastchat = 'huggingface'
+
+
+class Query(BaseModel):
+    query: str
+    collection_name: Repo
+
+
+class Answer(BaseModel):
+    answer: str
+    source_urls: list
